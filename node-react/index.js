51432e8388cea5c70cdb0e2006739ecd6696bd25
Mathieu Drapeau <m.drapeau9@gmail.com>
2019-02-12 12:35:03 -0500

    read update

--- node-react/index.js
+++ node-react/index.js
@@ -1,12 +1,73 @@
 /*
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(t => t.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed)
+    default:
+      throw new Error('Unknown filter: ' + filter)
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+})
+
+
+
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
-import App from './components/App'
+import PropTypes from 'prop-types'
+import Todo from './Todo'
+
+const TodoList = ({ todos, toggleTodo }) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+    ))}
+  </ul>
+)
+
+TodoList.propTypes = {
+  mdtmp: PropTypes.string.isRequired
+  mdtmpCb: PropTypes.func.isRequired
+}
+
+export default TodoList
+
+
+
 */
 'use strict'
+
+const MDTMPView = ({ todos, toggleTodo }) => (
+  <div>
+	test!!
+  </div>
+)
+
+MDTMPView.propTypes = {
+  mdtmp: PropTypes.string.isRequired,
+  mdtmpCb: PropTypes.func.isRequired
+}
+
+
+const mapStateToProps = state => ({
+  mdtmp: state.mdtmp
+})
+
+const mapDispatchToProps = dispatch => ({
+  mdtmpCb(){
+	console.log("mdtmp");
+  }
+})
+
+const MDTMP = ReactRedux.connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MDTMPView)
 
 
 const reducer = (state={}, action) => {
@@ -18,14 +79,12 @@ const reducer = (state={}, action) => {
   }
 }
 
-const store1= Redux.createStore(reducer)
+const store= Redux.createStore(reducer)
 
 
-/* mdtmp
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.render(
+  <React.Provider store={store}>
+    <MDTMP />
+  </React.Provider>,
   document.getElementById('root')
 )
-*/
