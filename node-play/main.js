39775c01657dfa7aeac4b7e650b3eff44a13b694
Mathieu Drapeau <m.drapeau9@gmail.com>
2019-02-18 18:28:41 -0500

    tmp

--- node-play/main.js
+++ node-play/main.js
@@ -1,19 +1,18 @@
 'use strict';
 
-
-
-
-const title = "Title!";
-PlaySet(title);
+const pathView1 = "view1/title"
+const title = "Title!"
+PlaySet(pathView1, title);
 //mdtmp set title.. (once/default value)
 //read it, set value in UI
 //attach set function to button
 
-const view = function(title)
+const view = function(path, title)
 {
+	console.log(path)
 	return `<p>${title}</p>
 		<button onclick=console.log("allo") type="button">Click Me!</button> 
-		<button onclick=PlaySet("buttonn2") type="button">Click Me 2!</button> 
+		<button onclick=PlaySet("${path}","buttonn2") type="button">Click Me 2!</button> 
 		`;
 }
 PlayInit("root", view)
--- node-play/play.js
+++ node-play/play.js
@@ -1,12 +1,55 @@
 'use strict';
 
-let staticState = undefined
+let staticModified = [] //mdtmp check duplicate??
+let staticState = {}
 let staticView = undefined
 let staticRoot = undefined
-function PlaySet(newState) {
-	staticState = newState
-	console.log(`PlaySet  : ${staticState}`)
-	PlayRender()
+
+function getLastElem(pathStr) {
+	//mdtmp duplicated code
+	const paths = pathStr.split("/")
+	if(paths.length===0) {return undefined}
+
+	const lastElementIdx = paths.length-1
+	let itrElem = staticState
+	{ // iterate to the deepest child
+		for(let i=0; i < lastElementIdx; ++i){
+			const propertyStr = paths[i]
+			if(!itrElem.hasOwnProperty(propertyStr)) {
+				// create parent property if doesn't exist
+				itrElem[propertyStr] = {}
+			}
+			itrElem = itrElem[propertyStr]
+		}
+	}
+	return itrElem[paths[lastElementIdx]]
+}
+
+function PlaySet(pathStr, newState) {
+	const paths = pathStr.split("/")
+	if(paths.length===0) {return}
+
+	const lastElementIdx = paths.length-1
+	let itrElem = staticState
+	{ // iterate to the deepest child
+		for(let i=0; i < lastElementIdx; ++i){
+			const propertyStr = paths[i]
+			if(!itrElem.hasOwnProperty(propertyStr)) {
+				// create parent property if doesn't exist
+				itrElem[propertyStr] = {}
+			}
+			itrElem = itrElem[propertyStr]
+		}
+	}
+
+	//mdtmp could push modified path for every change and
+	//pop change from path to view...
+	itrElem[paths[lastElementIdx]] = newState
+	staticModified.push(pathStr)
+
+	console.log("PlaySet  : ")
+	console.log(staticState)
+	PlayRender() // put render somewhere else?
 }
 
 function PlayInit(id, view) {
@@ -14,11 +57,14 @@ function PlayInit(id, view) {
 	staticRoot = document.getElementById("root")
 }
 function PlayRender() {
-	// mdtmp Could probably resolve this each time useing tag function...
-	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-	//
 	if(!staticState) return
 	if(!staticView) return
 	if(!staticRoot) return
-	staticRoot.innerHTML = staticView(staticState)
+
+	if(staticModified.length===0) return
+	const pathStr = staticModified[staticModified.length-1]
+	staticModified.pop()
+
+	const elem = getLastElem(pathStr)
+	staticRoot.innerHTML = staticView(pathStr, elem)
 }
