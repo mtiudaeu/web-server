084d260b2218eb62e36bf77c925058b7b344f5c8
Mathieu Drapeau <mdrapeau9@gmail.com>
2019-02-18 22:21:07 -0500

    update

--- node-play/main.js
+++ node-play/main.js
@@ -4,23 +4,23 @@ PlayInit("root")
 
 { // view 1!
 	const pathView1 = "view1/title"
-	const title1 = "Title 1"
+	const state = "Title 1"
 	const view = function(path, title)
 	{
 		return `<p>${title}</p>
 			<button onclick=PlaySetState("${path}","BuuUUUuuUUuuton1") type="button">Click Me 2!</button> 
 			`
 	}
-	PlaySetViewState(pathView1, view, title1)
+	PlaySetViewState(pathView1, view, state)
 }
 { // view 2!
 	const pathView2 = "view2"
-	const title2 = {title:"Title 2"}
+	const state = {title:"Title 2"}
 	const view2 = function(path, elem)
 	{
 		return `<p>${elem.title}</p>
 			<button onclick=PlaySetState("${path}",{title:"button2"}) type="button">Click Me 2!</button> 
 			`
 	}
-	PlaySetViewState(pathView2, view2, title2)
+	PlaySetViewState(pathView2, view2, state)
 }
