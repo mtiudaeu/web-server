73ec4338741301092ba9b843ca8c9f92985d2e51
Mathieu Drapeau <mdrapeau9@gmail.com>
2019-02-25 21:50:57 -0500

    update

--- node-play/main.js
+++ node-play/main.js
@@ -2,100 +2,10 @@
 
 PlayInit("root")
 
-{ // view 1!
-	const pathView1 = "view1/title"
-	const state = "Title 1"
-	const view = function(path, title)
-	{
-		return `<p>${title}</p>
-			<button onclick=PlaySetState("${path}","BuuUUUuuUUuuton1") type="button">Click Me 2!</button> 
-			`
-	}
-	PlaySetViewState(pathView1, view, state)
-}
-{ // view 2!
-	const pathView2 = "view2"
-	const state = {title:"Title 2"}
-	const view2 = function(path, elem)
-	{
-		return `<p>${elem.title}</p>
-			<button onclick=PlaySetState("${path}",{title:"button2"}) type="button">Click Me 2!</button> 
-			`
-	}
-	PlaySetViewState(pathView2, view2, state)
-}
-
-
-{ // cards
-	const pathView3 = "view3"
-	const state3 = {}
-	const view3 = function(path, state)
-	{
-return `<div class="card-columns">
-		  <div class="card">
-		    <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap">
-		    <div class="card-body">
-		      <h5 class="card-title">Card title that wraps to a new line</h5>
-		      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-		    </div>
-		  </div>
-		  <div class="card p-3">
-		    <blockquote class="blockquote mb-0 card-body">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-		      <footer class="blockquote-footer">
-		        <small class="text-muted">
-		          Someone famous in <cite title="Source Title">Source Title</cite>
-		        </small>
-		      </footer>
-		    </blockquote>
-		  </div>
-		  <div class="card">
-		    <img class="card-img-top" src="https://via.placeholder.com/300x90.png" alt="Card image cap">
-		    <div class="card-body">
-		      <h5 class="card-title">Card title</h5>
-		      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-		      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		    </div>
-		  </div>
-		  <div class="card bg-primary text-white text-center p-3">
-		    <blockquote class="blockquote mb-0">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-		      <footer class="blockquote-footer">
-		        <small>
-		          Someone famous in <cite title="Source Title">Source Title</cite>
-		        </small>
-		      </footer>
-		    </blockquote>
-		  </div>
-		  <div class="card text-center">
-		    <div class="card-body">
-		      <h5 class="card-title">Card title</h5>
-		      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-		      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		    </div>
-		  </div>
-		  <div class="card">
-		    <img class="card-img" src="..." alt="Card image">
-		  </div>
-		  <div class="card p-3 text-right">
-		    <blockquote class="blockquote mb-0">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-		      <footer class="blockquote-footer">
-		        <small class="text-muted">
-		          Someone famous in <cite title="Source Title">Source Title</cite>
-		        </small>
-		      </footer>
-		    </blockquote>
-		  </div>
-		  <div class="card">
-		    <div class="card-body">
-		      <h5 class="card-title">Card title</h5>
-		      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-		      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-		    </div>
-		  </div>
-		</div>`
-	}
-	PlaySetViewState(pathView3, view3, state3)
-}
+const pathView1 = "view1/title"
+PlaySetViewState(pathView1, view1, state1)
+const pathView2 = "view2"
+PlaySetViewState(pathView2, view2, state2)
+const pathView3 = "view3"
+PlaySetViewState(pathView3, view3, state3)
 
---
+++ node-play/state.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const state1 = "Title 1"
+const state2 = {title:"Title 2"}
+const state3 = {}
