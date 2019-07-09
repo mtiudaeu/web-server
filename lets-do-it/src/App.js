b9d2ca5e3f40dd646f6a7a006003d2d88ab0fc25
Mathieu Drapeau <m.drapeau9@gmail.com>
2019-07-08 23:36:55 -0400

    Moved app to parent + add a hook example

--- lets-do-it/src/App.js
+++ lets-do-it/src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -18,6 +33,7 @@ function App() {
         >
           Learn React
         </a>
+		<Example/>
       </header>
     </div>
   );
