91211c3b8aec69cf0d51dd623679e80951d84fa9
Mathieu Drapeau <m.drapeau9@gmail.com>
2019-07-21 18:08:32 -0400

    update

--- lets-do-it/src/App.js
+++ lets-do-it/src/App.js
@@ -2,41 +2,88 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//mdmtp
+/*
+   -GlobalState
+   headEntity
+   arrayEntities
+   deletedEntities
 
-function Example() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+   -CurrentViewer
+//displaycurrentNode
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
+-nodeComponent
+Connections
+Views(...)
+
+*/
+
+function SimpleView(props){
+
+	}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-		<Example/>
-      </header>
-    </div>
-  );
+
+	const [globalState, setGlobalState] = useState(
+		{
+			headEntity: null,
+			arrayEntities: [],
+			deletedEntities: []
+		});
+
+	const addNode = () => {
+
+		const id = globalState.arrayEntities.length
+		const newEntity = {
+			id:id,
+			components:[]
+		}
+
+		globalState.arrayEntities.push(newEntity)
+		setGlobalState(globalState)
+	};
+
+	const setConnection = (props) => {
+		const COMP_CONN = "compConnections"
+
+		const id1, id2 = {...props}
+		const entity1 = globalState.arrayEntities[id1]
+		const entity2 = globalState.arrayEntities[id2]
+
+		if(!entity1[COMP_CONN]) {
+			entity1[COMP_CONN] = []
+		}
+		if(!entity2[COMP_CONN]) {
+			entity2[COMP_CONN] = []
+		}
+		entity1[COMP_CONN].push(id2)
+		entity2[COMP_CONN].push(id1)
+
+		setGlobalState(globalState)
+	}
+	//FIXME delete connection (look above)
+
+	const setView = (props) => {
+		const COMP_VIEW = "compSimpleView"
+
+		const id, title = {...props}
+		const entity = globalState.arrayEntities[id]
+		if(!entity[COMP_VIEW]) {
+			entity[COMP_VIEW] = []
+		}
+		entity[COMP_VIEW].title = title
+
+		setGlobalState(globalState)
+	}
+
+
+	return (
+		<div className="App">
+		<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		</header>
+		</div>
+	);
 }
 
 export default App;
