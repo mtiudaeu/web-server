aacd189b63fb31b63d8e7303c362132aaa838748
Mathieu Drapeau <mdrapeau@stingray.com>
2019-08-30 12:50:25 -0400

    update

--- cpp_woohoo/src/main.cpp
+++ cpp_woohoo/src/main.cpp
@@ -2,29 +2,35 @@
 
 #include <iostream>
 #include <fstream>
+#include <functional>
 
 struct WriteData {
   std::string path;
   std::string valueToWrite;
 };
-
-eh::Ret writeTestInFile(WriteData input) {
+std::function<eh::Ret(WriteData)> writeTestInFile = [](WriteData input) {
   std::ofstream fs;
   fs.open(input.path, std::ios::in);
   if(!fs.is_open()){
-    return RET_ERROR(input.path, "doesn't exist");
+    RET_ERROR(input.path, "doesn't exist");
 
   }
   fs << input.valueToWrite;
   fs.close();
 
   return RET_SUCCESS();
+};
+
+template<typename Out, typename In>
+Out callFunc(std::function<Out(In)> func, In& input) {
+  return func(input);
 }
 
 int main() {
-
   WriteData data{.path="tmp.txt", .valueToWrite="test\n"};
-  eh::Ret ret = writeTestInFile(data);
+//mdtmp We can build a pipeline that way and them start the application.
+// we will then be able to inspect it.
+  eh::Ret ret = callFunc<eh::Ret, WriteData>(writeTestInFile, data);
   if(ret.error){
     LOG_ERROR(ret);
     return 1;
