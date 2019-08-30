eb6155c45d756e2ef5c807347f9a3a900099ba93
Mathieu Drapeau <mdrapeau@stingray.com>
2019-08-30 14:09:13 -0400

    update

--- cpp_woohoo/src/main.cpp
+++ cpp_woohoo/src/main.cpp
@@ -12,8 +12,7 @@ std::function<eh::Ret(WriteData)> writeTestInFile = [](WriteData input) {
   std::ofstream fs;
   fs.open(input.path, std::ios::in);
   if(!fs.is_open()){
-    RET_ERROR(input.path, "doesn't exist");
-
+    return RET_ERROR(input.path, "doesn't exist");
   }
   fs << input.valueToWrite;
   fs.close();
@@ -26,15 +25,37 @@ Out callFunc(std::function<Out(In)> func, In& input) {
   return func(input);
 }
 
-int main() {
-  WriteData data{.path="tmp.txt", .valueToWrite="test\n"};
-//mdtmp We can build a pipeline that way and them start the application.
-// we will then be able to inspect it.
-  eh::Ret ret = callFunc<eh::Ret, WriteData>(writeTestInFile, data);
-  if(ret.error){
-    LOG_ERROR(ret);
-    return 1;
+struct Pipeline{
+  std::vector<std::function<eh::Ret()>> funcs;
+};
+void pipelineAdd(Pipeline& pipeline, std::function<eh::Ret()> func){
+  pipeline.funcs.push_back(func);
+}
+eh::Ret pipelineRun(Pipeline& pipeline){
+  for(auto it:pipeline.funcs){
+    eh::Ret ret = it();
+    if(ret.error) {
+      return ret;
+    } 
   }
+  return RET_SUCCESS();
+}
+
+
+int main() {
+  Pipeline pipeline;
+
+  std::function<eh::Ret()> func = []() {
+    WriteData data{.path="tmp.txt", .valueToWrite="test\n"};
+    return callFunc<eh::Ret, WriteData>(writeTestInFile, data);
+  };
+
+  pipelineAdd(pipeline, func);
+  eh::Ret ret = pipelineRun(pipeline);
+  if(ret.error) {
+    LOG_ERROR(ret.msg);
+    return 1;
+  } 
 
   return 0;
 }
