dfda3690aac1c34f6c3c681246413c4de4589a80
Mathieu Drapeau <mdrapeau@stingray.com>
2019-08-30 14:43:25 -0400

    update

--- cpp_woohoo/src/main.cpp
+++ cpp_woohoo/src/main.cpp
@@ -8,16 +8,24 @@ struct WriteData {
   std::string path;
   std::string valueToWrite;
 };
-std::function<eh::Ret(WriteData)> writeTestInFile = [](WriteData input) {
+
+struct PipelineData {
+  eh::Ret ret;
+};
+
+std::function<PipelineData(WriteData)> writeTestInFile = [](WriteData input) {
   std::ofstream fs;
   fs.open(input.path, std::ios::in);
   if(!fs.is_open()){
-    return RET_ERROR(input.path, "doesn't exist");
+
+    PipelineData data;
+    data.ret = RET_ERROR(input.path, "doesn't exist");
+    return data;
   }
   fs << input.valueToWrite;
   fs.close();
 
-  return RET_SUCCESS();
+  return PipelineData();
 };
 
 template<typename Out, typename In>
@@ -25,17 +33,18 @@ Out callFunc(std::function<Out(In)> func, In& input) {
   return func(input);
 }
 
+
 struct Pipeline{
-  std::vector<std::function<eh::Ret()>> funcs;
+  std::vector<std::function<PipelineData()>> funcs;
 };
-void pipelineAdd(Pipeline& pipeline, std::function<eh::Ret()> func){
+void pipelineAdd(Pipeline& pipeline, std::function<PipelineData()> func){
   pipeline.funcs.push_back(func);
 }
 eh::Ret pipelineRun(Pipeline& pipeline){
   for(auto it:pipeline.funcs){
-    eh::Ret ret = it();
-    if(ret.error) {
-      return ret;
+    PipelineData data = it();
+    if(data.ret.error) {
+      return data.ret;
     } 
   }
   return RET_SUCCESS();
@@ -45,9 +54,9 @@ eh::Ret pipelineRun(Pipeline& pipeline){
 int main() {
   Pipeline pipeline;
 
-  std::function<eh::Ret()> func = []() {
+  std::function<PipelineData()> func = []() {
     WriteData data{.path="tmp.txt", .valueToWrite="test\n"};
-    return callFunc<eh::Ret, WriteData>(writeTestInFile, data);
+    return callFunc<PipelineData, WriteData>(writeTestInFile, data);
   };
 
   pipelineAdd(pipeline, func);
