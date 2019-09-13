751f5e973b909d770a70a97f4d01ee9b282c590b
Mathieu Drapeau <mdrapeau@stingray.com>
2019-09-13 12:49:22 -0400

    works

--- java-01/src/com/mtiudaeu/Main.java
+++ java-01/src/com/mtiudaeu/Main.java
@@ -95,6 +95,20 @@ public class Main {
         public String path;
         public String valueToWrite;
     }
+    static public class AddPathToDataToWrite extends Pipeline2<String,DataToWrite> {
+        private String path;
+        public AddPathToDataToWrite(String path){
+            this.path = path;
+        }
+        public PipelineData<DataToWrite> run(String valueToWrite) {
+            PipelineData ret = new PipelineData();
+            DataToWrite data = new DataToWrite();
+            data.path = this.path;
+            data.valueToWrite = valueToWrite;
+            ret.data = data;
+            return ret;
+        }
+    }
     static public class WriteToFile extends Pipeline2<DataToWrite,Object> {
         public PipelineData<Object> run(DataToWrite data) {
             PipelineData ret = new PipelineData();
@@ -147,9 +161,11 @@ public class Main {
                 value += "::\n";
 
                 // Keywait line
-                value += "KeyWait ";
-                value += it.keyWaitKeyCodes;
-                value += "\n";
+                if(it.keyWaitKeyCodes!=null){
+                    value += "KeyWait ";
+                    value += it.keyWaitKeyCodes;
+                    value += "\n";
+                }
 
                 // mdtmp comment
                 value += "if(State = 2)\n";
@@ -176,7 +192,7 @@ public class Main {
         }
     }
 
-    //mdtmp
+
     static public class AutoKeyMerge extends PipelineMerge3<String,String,DataToWrite> {
         public PipelineData<DataToWrite> run(String input1, String input2) {
             PipelineData ret = new PipelineData();
@@ -191,28 +207,38 @@ public class Main {
 
 
     public static void main(String[] args) {
-         class DataOutputPath extends Pipeline2<String,DataToWrite> {
-            public PipelineData<DataToWrite> run(String valueToWrite) {
-                //mdtmp hardcoded
-                String path = "out/tmp.txt";
 
-                PipelineData ret = new PipelineData();
-                DataToWrite data = new DataToWrite();
-                data.path = path;
-                data.valueToWrite = valueToWrite;
-                ret.data = data;
-                return ret;
-            }
-        }
+        AddPathToDataToWrite path = new AddPathToDataToWrite("out/tmp.txt");
 
         AutoKeyWrap wrap = new AutoKeyWrap();
-        Pipeline3<AutoKeyArray, String, DataToWrite> prepareToWrite = new Pipeline3<AutoKeyArray, String, DataToWrite>(wrap, new DataOutputPath());
+        Pipeline3<AutoKeyArray, String, DataToWrite> prepareToWrite = new Pipeline3<AutoKeyArray, String, DataToWrite>(wrap, path);
         Pipeline3<AutoKeyArray, DataToWrite, Object> finalPipeline = new Pipeline3<AutoKeyArray, DataToWrite, Object>(prepareToWrite, new WriteToFile());
 
         DefaultPipeline<AutoKeyArray, Object> pipeline = new DefaultPipeline<AutoKeyArray, Object>(finalPipeline);
         try {
             AutoKeyArray keyInputs = new AutoKeyArray();
-            keyInputs.keys.add(new AutoKeyArray.Keys("1", "1", "1"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("0", "0", "0"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("1", "1", "z"));
+            for(Integer i=2; i<10; i++) {
+                keyInputs.keys.add(new AutoKeyArray.Keys(i.toString(), i.toString(), i.toString()));
+            }
+            for(Integer i=0; i<10; i++) {
+                keyInputs.keys.add(new AutoKeyArray.Keys("+"+i.toString(), i.toString(), "{Shift down}"+i.toString()+"{Shift up}"));
+            }
+            for(Integer i=1; i<6; i++) {
+                keyInputs.keys.add(new AutoKeyArray.Keys("F"+i.toString(), "F"+i.toString(), "{F"+i.toString()+"}"));
+            }
+
+            //mouvement key
+            keyInputs.keys.add(new AutoKeyArray.Keys("Up", null, "{w down}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Up up", null, "{w up}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Down", null, "{s down}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Down up", null, "{s up}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Left", null, "{a down}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Left up", null, "{a up}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Right", null, "{d down}"));
+            keyInputs.keys.add(new AutoKeyArray.Keys("Right up", null, "{d up}"));
+
             PipelineData<Object> output = pipeline.runDefault(keyInputs);
         } catch(Exception e) {
             e.printStackTrace();
