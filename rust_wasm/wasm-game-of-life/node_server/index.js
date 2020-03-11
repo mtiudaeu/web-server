eaa06e22375e6d0b5d7d5f0bf5e3685c1db769c7
Mathieu Drapeau <mdrapeau@stingray.com>
2020-03-11 15:25:12 -0400

    update rust_wasm

--- rust_wasm/wasm-game-of-life/node_server/index.js
+++ rust_wasm/wasm-game-of-life/node_server/index.js
@@ -1,3 +1,116 @@
 import * as wasm from "wasm-game-of-life";
 
-wasm.greet();
+var input = `
+a
+b
+c
+d
+e
+f
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0
+Up,null,{w down}
+Up up,null,{w up}
+Down,null,{s down}
+Down up,null,{s up}
+Left,null,{a down}
+Left up,null,{a up}
+Right,null,{d down}
+Right up,null,{d up}
++f,f,{Shift down}f{Shift up}
++t,t,{Shift down}t{Shift up}
++g,g,{Shift down}g{Shift up}
++y,y,{Shift down}y{Shift up}
++F1,F1,{Shift down}{F1}{Shift up}
++F2,F2,{Shift down}{F2}{Shift up}
++F3,F3,{Shift down}{F3}{Shift up}
++F4,F4,{Shift down}{F4}{Shift up}
++F5,F5,{Shift down}{F5}{Shift up}
+F1,F1,{F1}
+F2,F2,{F2}
+F3,F3,{F3}
+F4,F4,{F4}
+F5,F5,{F5}
+`;
+
+
+var template = `
+~#input_key_code#::
+KeyWait  #key_wait_key_code#
+if(State = 1)
+{
+        IfWinActive ahk_id %wowid1%
+        {
+                ControlSend,,  #output_key_code#, ahk_id %wowid1%
+        }
+        IfWinActive ahk_id %wowid2%
+        {
+                ControlSend,,  #output_key_code#, ahk_id %wowid2%
+        }
+}
+if(State = 2)
+{
+        IfWinActive ahk_id %wowid1%
+        {
+                ControlSend,,  #output_key_code#, ahk_id %wowid2%
+        }
+        IfWinActive ahk_id %wowid2%
+        {
+                ControlSend,,  #output_key_code#, ahk_id %wowid1%
+        }
+}
+if(State = 3)
+{
+        ControlSend,,  #output_key_code#, ahk_id %wowid1%
+        ControlSend,,  #output_key_code#, ahk_id %wowid2%
+}
+Return
+`;
+
+
+wasm.greet(input, template);
+
+
+//mdtmp
+// 1- get id for (input) text box one
+// 2- get id for (output) text box one
+// 2- get id from a button
+// 4- prefill input with autokey input config
+// 5- add listener to a button
+// 6- listener -> run a rust algorithm
+//    -> pass string instead of reading file.
+
+// Maybe a lot of compilation error.... don't know
+
+
+
+
+
+
--- rust_wasm/wasm-game-of-life/src/lib.rs
+++ rust_wasm/wasm-game-of-life/src/lib.rs
@@ -14,6 +14,84 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, allo!");
+pub fn greet(input: &str, template: &str) {
+//mdtmp use template
+    alert(&format!("Hello, {}!", input));
 }
+
+//----- AutokeyKey
+struct AutokeyKey {
+  input_key_code: String,
+  key_wait_key_code: String,
+  output_key_code: String
+}
+fn create_autokey_key (
+  input_key_code: String,
+  key_wait_key_code: String,
+  output_key_code: String ) -> AutokeyKey {
+  return AutokeyKey{
+    input_key_code: input_key_code.to_string(),
+    key_wait_key_code: key_wait_key_code.to_string(),
+    output_key_code: output_key_code.to_string()
+  };
+}
+
+//mdtmp move all the code below up
+//      and change reading files to using the input str 
+
+/*
+
+
+//----- resolve_to_autokey_syntax
+fn resolve_to_autokey_syntax (filename: &'static str, key_array: Vec<AutokeyKey>) -> String {
+  let input_template = fs::read_to_string(filename).expect("Unable to read file");
+
+  let mut ret : String = String::new();
+  for key in key_array.iter() {
+    let mut tmp = input_template.clone();
+
+    //HACK!!
+    if key.key_wait_key_code == "null" {
+      tmp = tmp.replace("KeyWait  #key_wait_key_code#\n", "");
+    }
+    tmp = tmp.replace("#input_key_code#",key.input_key_code.as_str());
+    tmp = tmp.replace("#key_wait_key_code#",key.key_wait_key_code.as_str());
+    tmp = tmp.replace("#output_key_code#",key.output_key_code.as_str());
+
+    ret += tmp.as_str();
+  }
+
+  return ret;
+}
+
+
+
+fn create_autokey_array_from_file(filename: &'static str) -> Vec<AutokeyKey> {
+  let mut key_array : Vec<AutokeyKey> = Vec::new();
+
+  let data = fs::read_to_string(filename).expect("Unable to read file");
+  for line in data.lines() {
+    let keys_str : Vec<&str> = line.split(",").collect();
+    if keys_str.len() ==1 {
+      key_array.push(
+        create_autokey_key(keys_str[0].to_string(), keys_str[0].to_string(), keys_str[0].to_string()));
+    }
+    else if keys_str.len() ==3 {
+      key_array.push(
+        create_autokey_key(keys_str[0].to_string(), keys_str[1].to_string(), keys_str[2].to_string()));
+    }
+    else {
+       println!("Wrong number of parameter for line : {}", line);
+    }
+  }
+
+  return key_array;
+}
+
+
+
+fn main() {
+  let key_array = create_autokey_array_from_file("keys.txt");
+  let value = resolve_to_autokey_syntax("template.txt", key_array);
+  fs::write("foo.txt", value).expect("Unable to write file");
+}*/
